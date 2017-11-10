52ff08a35ae540eb2b0ec2acb7a183a4815c7b9f
misspia <pia_leung@hotmail.com>
2017-11-10 01:20:21 -0500

    init profile saga, add story types to reducer

--- src/profile/profile.reducer.js
+++ src/profile/profile.reducer.js
@@ -5,8 +5,11 @@ const initialState = {
 
 export default(state = initialState, action) => {
 	switch(action.type) {
-		case TEST:
+		case FETCH_PROFILE:
 			return state;
+		case FETCH_PROFILE_SUCCEEDED: 
+			console.log('FETCH PROFILE SUCCESS -- profile.reducer');
+			return { ...state, name: 'pkname' };
 		default:
 			return state;
 	}
