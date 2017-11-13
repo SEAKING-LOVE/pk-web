f0e6776e1cc82818307185adcf0dff24fa73c868
Jason Manson-Hing <jason.mansonhing@gmail.com>
2017-11-12 21:17:08 -0800

    Split up webpack

---
+++ webpack.dev.js
@@ -0,0 +1,16 @@
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+	plugins: [
+		new webpack.DefinePlugin({
+			API_HOST: JSON.stringify('http://localhost:3000')
+		}),
+		new webpack.DefinePlugin({
+			'process.env': {
+				'NODE_ENV': JSON.stringify('development')
+			}
+		})
+	]
+});
