b0c02bfe7a39035ad0d2052886a445922cf511a3
Jason Manson-Hing <jason.mansonhing@gmail.com>
2017-11-12 22:40:27 -0800

    Simple push to force rebuild

--- server.js
+++ server.js
@@ -3,4 +3,6 @@ var app = express();
 
 app.use(express.static(__dirname + '/dist'));
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080, () => {
+	console.log('Server started')
+});
