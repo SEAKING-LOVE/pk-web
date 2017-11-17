49bc27add01817133cb41be0fd9a0e61c6ceae93
misspia <pia_leung@hotmail.com>
2017-11-17 14:50:29 -0500

    render move component tabs and selected data

--- src/profile/moves/moves.jsx
+++ src/profile/moves/moves.jsx
@@ -1,13 +1,45 @@
 import React, { Component } from 'react';
 
 class Moves extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			tabIndex: 0
+		};
+		this.tabs = [
+			{label: 'machine', data: props.machine},
+			{label: 'level up', data: props.levelUp},
+			{label: 'egg', data: props.egg},
+			{label: 'tutor', data: props.tutor},
+		];
+	}
+	renderTabs() {
+		const tabs = this.tabs.map((tab, index) => {
+			return this.renderTab(tab.label, index);
+		});
+		return <div className='tabs'>
+			{tabs}
+		</div>
+	}
+	renderTab(label, index) {
+		return <div
+			key={index}
+			onClick={() => {this.selectTab(index)}}
+			className={this.state.tabIndex === index ? 'active' : ''}>
+			{label}
+		</div>
+	}
+	selectTab(index) {
+		this.setState({ tabIndex: index });
+	}
+	renderMoves() {
+		return JSON.stringify(this.tabs[this.state.tabIndex].data);
+	}
 	render() {
 		return <div className='moves'>
 			Moves component
-			<div>{JSON.stringify(this.props.machine)}</div>
-			<div>{JSON.stringify(this.props.levelUp)}</div>
-			<div>{JSON.stringify(this.props.egg)}</div>
-			<div>{JSON.stringify(this.props.tutor)}</div>
+			{this.renderTabs()}
+			{this.renderMoves()}
 		</div>
 	}
 }
