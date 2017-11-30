e0113d6092f71feedcf82003483ff723c9085b0b
misspia <pia_leung@hotmail.com>
2017-11-29 22:00:17 -0500

    move stat addition to reduce fn, maps to sub-render functions

--- src/profile/stats/stats.jsx
+++ src/profile/stats/stats.jsx
@@ -2,38 +2,52 @@ import React, { Component } from 'react';
 import { StatsContainer, Row, Background, Bar, TotalBar, Divider, ProgressName, ProgressBar } from './stats.styles.jsx'
 
 class Stats extends Component {
+	renderStatBars() {
+		return this.props.stats.map( stat => {
+			return this.renderStatBar(stat.name, stat.value);
+		});
+	}
+	renderStatBar(label, value) {
+		const width = `${value * 100 / 255}%`;
+		return <Row key={label}>
+				<ProgressName> {label} </ProgressName>
+				<ProgressBar>
+					<Background>
+						<Bar style={{ width }}> {value}</Bar>
+					</Background>
+				</ProgressBar>
+			</Row>
+	}
+	renderTotalSection() {
+		return <Row>
+			<ProgressName>
+				Total
+			</ProgressName>
+			<ProgressBar>
+				<Background>
+					{this.renderTotalBar()}
+				</Background>
+			</ProgressBar>
+		</Row>
+	}
+	renderTotalBar() {
+		const totalStats = this.sumStats();
+		return <TotalBar style={{width: totalStats * 100 / 1530 + '%'}}>
+			{totalStats}
+		</TotalBar>;
+	}
+	sumStats() {
+		return this.props.stats.reduce((acc, curr) => {
+			return acc + curr.value;
+		}, 0);
+	}
 	render() {
 		let totalStats = 0;
 
 		return <StatsContainer className='stats'>
-			{
-				this.props.stats.map( stat => {
-					totalStats += stat.value
-					return (
-						<Row key={stat.name}>
-							<ProgressName>
-								{stat.name}
-							</ProgressName>
-							<ProgressBar>
-								<Background>
-									<Bar style={{width: stat.value * 100 / 255 + '%'}}> {stat.value}</Bar>
-								</Background>
-							</ProgressBar>
-						</Row>
-					);
-				})
-			}
+			{this.renderStatBars()}
 			<Divider />
-			<Row>
-				<ProgressName>
-					Total
-				</ProgressName>
-				<ProgressBar>
-					<Background>
-						<TotalBar style={{width: totalStats * 100 / 1530 + '%'}}> {totalStats}</TotalBar>
-					</Background>
-				</ProgressBar>
-			</Row>
+			{this.renderTotalSection()}
 		</StatsContainer>
 	}
 }
