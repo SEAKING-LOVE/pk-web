35b7f2cc87e0909d9d0d2812d4126dc84e3fa0a8
misspia <pia_leung@hotmail.com>
2017-11-29 21:41:08 -0500

    add type themes to color sheet

--- src/themes/colors.js
+++ src/themes/colors.js
@@ -11,6 +11,62 @@ const Colors = {
 	gradient: {
 		pink: 'linear-gradient(to bottom, #db3463, #d92a47);'
 	},
+	types: {
+		normal: (alpha=1.0) => { 
+			return `rgba( 156, 160, 113 , ${alpha})`
+		},
+		fire: (alpha=1.0) => {
+			return `rgba( 246, 97, 79, ${alpha})`
+		},
+		water: (alpha=1.0) => {
+			return `rgba( 0, 204, 255, ${alpha})`
+		}, 
+		electric: (alpha=1.0) => {
+			return `rgba( 242, 205, 60, ${alpha})`
+		}, 
+		grass: (alpha=1.0) => {
+			return `rgba( 113, 188, 120, ${alpha})`
+		},
+		ice: (alpha=1.0) => {
+			return `rgba( 164, 244, 249, ${alpha})`
+		},
+		fighting: (alpha=1.0) => {
+			return `rgba( 164, 244, 249, ${alpha})`
+		},
+		poison: (alpha=1.0) => {
+			return `rgba( 138, 73, 107, ${alpha})`
+		},
+		ground: (alpha=1.0) => {
+			return `rgba( 166, 123, 91, ${alpha})`
+		}, 
+		flying: (alpha=1.0) => {
+			return `rgba( 206, 200, 239, ${alpha})`
+		},
+		psychic: (alpha=1.0) => {
+			return `rgba( 222, 111, 161, ${alpha})`
+		},
+		bug: (alpha=1.0) => {
+			return `rgba( 208, 206, 28, ${alpha})`
+		}, 
+		rock: (alpha=1.0) => {
+			return `rgba( 190, 190, 190, ${alpha})`
+		},
+		ghost: (alpha=1.0) => {
+			return `rgba( 150, 120, 182, ${alpha})`
+		},
+		dragon: (alpha=1.0) => {
+			return `rgba( 15, 77, 146, ${alpha})`
+		},
+		dark: (alpha=1.0) => {
+			return `rgba( 28, 40, 65, ${alpha})`
+		}, 
+		steel: (alpha=1.0) => {
+			return `rgba( 162, 173, 208, ${alpha})`
+		},
+		fairy: (alpha=1.0) => {
+			return `rgba( 255, 192, 203, ${alpha})`
+		},	
+	},
 	
 }
 
