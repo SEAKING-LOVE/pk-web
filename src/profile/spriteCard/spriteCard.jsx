c4d901a87df24bc8b2feee05fc05d6ef3ca19ed5
misspia <pia_leung@hotmail.com>
2017-12-04 23:25:50 -0500

    render name,formatted id, list of types

--- src/profile/spriteCard/spriteCard.jsx
+++ src/profile/spriteCard/spriteCard.jsx
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
+import { leadingZeros } from '../../utils.js';
 
 class SpriteCard extends Component {
+	renderHeader() {
+		return <div>
+			{leadingZeros(this.props.id)} {this.props.name}
+			{this.renderTypes()}
+		</div>
+	}
+	renderTypes() {
+		const types = this.props.types.map((type, index) => {
+			return <div key={index}>{type.name}</div>
+		});
+		return <div>{types}</div>
+	}
+	renderArtwork() {
+		return <img src={this.props.artwork} alt={`${this.props.name} artwork`}/>
+	}
+	renderSprite() {
+		return <img src={this.props.sprite} alt={`${this.props.name} sprite`}/>
+	}
 	render() {
 		return <div>
-			sprite card component
-			<img src={this.props.artwork} alt={`${this.props.name} artwork`}/>
+			{this.renderHeader()}
+			{this.renderArtwork()}
+			{this.renderSprite()}
 		</div>
 	}
 }
