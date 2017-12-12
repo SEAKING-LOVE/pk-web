1197c904bd0cffc1d4614b4bea91225e9013fd23
misspia <pia_leung@hotmail.com>
2017-12-12 17:34:16 -0500

    rearrange overview table + swap id out for catch rate

--- src/profile/basics/overview/overview.jsx
+++ src/profile/basics/overview/overview.jsx
@@ -1,43 +1,47 @@
 import React, { Component } from 'react';
 import { leadingZeros } from '../../../utils.js';
-import { OverviewTable, Row, Cell, Title, Subtitle } from './overview.styles.jsx'
+import { OverviewTable, Row, Cell, Title, Subtitle, smallSubtitle } from './overview.styles.jsx'
 
 class Overview extends Component {
 	renderAbilities() {
-		return this.props.abilities.map( ability => {
-			return <div key={ability.id}>{ability.name}</div>
-		})
+		return this.props.abilities.map( ability => this.renderAbility(ability))
+	}
+	renderAbility(ability) {
+		return <Subtitle key={ability.id} className={ability.hidden ? smallSubtitle : ''}>
+			{ability.name} {ability.hidden ? '(hidden)' : ''}
+		</Subtitle>
 	}
 	renderOverviewTable() {
 		return <OverviewTable className='overview'>
 			<Row>
 				<Cell>
-					<Subtitle>{leadingZeros(this.props.id)}</Subtitle>
-					<Title>National No</Title>
+					<Subtitle>{this.props.height}M</Subtitle>
+					<Title>Height</Title>
 				</Cell>
+				<Cell>
+					<Subtitle>{this.props.weight}KG</Subtitle>
+					<Title>Weight</Title>
+				</Cell>
+				<Cell>
+					<Subtitle>{this.props.captureRate}</Subtitle>
+					<Title>catch rate</Title>
+				</Cell>	
+			</Row>
+			<Row>
 				<Cell>
 					<Subtitle>{this.renderAbilities()}</Subtitle>
 					<Title>Ability</Title>
+				</Cell>
+				<Cell>
+					<Subtitle>{this.props.baseExperience}</Subtitle>
+					<Title>Base Exp</Title>
 				</Cell>
 				<Cell>
 					<Subtitle> {this.props.baseHappiness} </Subtitle>
 					<Title>Base Happiness</Title>
 				</Cell>
 			</Row>
-			<Row>
-				<Cell>
-					<Subtitle>{this.props.height}m</Subtitle>
-					<Title>Height</Title>
-				</Cell>
-				<Cell>
-					<Subtitle>{this.props.weight}kg</Subtitle>
-					<Title>Weight</Title>
-				</Cell>
-				<Cell>
-					<Subtitle>{this.props.baseExperience}</Subtitle>
-					<Title>Base Exp</Title>
-				</Cell>
-			</Row>
+			
 		</OverviewTable>
 	}
 	render() {
