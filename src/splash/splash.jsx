1e16349022b79c354cd40d8aac93ef8ffdce61f6
misspia <pia_leung@hotmail.com>
2018-01-08 16:19:07 -0500

    init splash screen component

---
+++ src/splash/splash.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Splash extends Component {
+	render() {
+		return <div>
+			spash screen
+		</div>
+	}
+}
+
+export default Splash;
---
+++ src/splash/splash.styles.js
@@ -0,0 +1,6 @@
+import styled, { css } from 'react-emotion';
+import { Colors } from '../themes/themes.js';
+
+const Container = styled('div')`
+	
+`;
